18a38214b68c7364da5dcfad4c86511f8520a4b7
LeeJaeLee <cuonggaayf01@gmail.com>
2022-05-18 10:38:21 +0700

    PTIT

---
+++ Operators_and_ConditionVariables/main.js
@@ -0,0 +1,14 @@
+var myObject = {
+    name: "Duc Cuong",
+    age: 18,
+    adress: "Ha Noi",
+    myFunction: function(){
+
+    }
+};
+
+console.log('myObject', myObject);
+
+var myFun = function(){
+    alert("Hello")
+}
---
+++ variables_comments_builtin/main.js
@@ -0,0 +1,5 @@
+alert("HelloWorld")
+
+setTimeout(function(){
+    alert("Thông báo")
+}, 1000)
