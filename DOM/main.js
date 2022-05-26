3bbbb376dee93ea251c37234b7a99fae0d0560ec
LeeJaeLee <cuonggaayf01@gmail.com>
2022-05-26 22:57:04 +0700

    PTIT

--- DOM/main.js
+++ DOM/main.js
@@ -1,6 +1,13 @@
 /*
 html dom co 3 thanh phan
 element
-attribute
-text
+attribute: hreft
+text: "Hello"
+
 */
+
+var h1ele = document.querySelector('h1')
+
+h1ele.title = 'hello'
+
+h1ele.getAttribute('class')
