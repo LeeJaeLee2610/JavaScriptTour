594253c55ad4f564b3467afda4428177a7998768
LeeJaeLee <cuonggaayf01@gmail.com>
2022-05-27 23:27:23 +0700

    PTIT

--- DOM/main.js
+++ DOM/main.js
@@ -3,11 +3,18 @@ html dom co 3 thanh phan
 element
 attribute: hreft
 text: "Hello"
-
+innerText: Chỉ hiện những gì thấy
+textContent: Hiện hết tất cả kể cả dính display none
+innerHTML: Những thẻ tag thì sẽ tự động chuyển 
 */
 
-var h1ele = document.querySelector('h1')
+var h1Demo = document.querySelector(".h1_demo")
 
-h1ele.title = 'hello'
+console.log(h1Demo.getAttribute("class"))
 
-h1ele.getAttribute('class')
+document.querySelector(".heading").innerHTML = "OK"
+
+console.log(document.querySelector(".heading").innerHTML)
+
+var boxEle = document.querySelector('.box')
+console.log([boxEle])
