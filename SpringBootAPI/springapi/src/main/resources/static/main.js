c674b267818d1ced226d5a15a5b87f4f41714d49
LeeJaeLee <cuonggaayf01@gmail.com>
2022-06-01 12:44:53 +0700

    PTIT

--- SpringBootAPI/springapi/src/main/resources/static/main.js
+++ SpringBootAPI/springapi/src/main/resources/static/main.js
@@ -2,6 +2,7 @@ var getApi =  "http://localhost:8080/rest/getStudents"
 
 function start(){
     getStudents(renderStudents)
+    handleCreateForm();
 }
 
 start()
@@ -24,4 +25,40 @@ function renderStudents(students){
         </li>`
     })
     listStudents.innerHTML = htmls.join("");
+}
+
+// document.querySelector("#create").addEventListener('click', () =>{
+//     alert("Click ne")
+// })
+
+function handleCreateForm(){
+    var createBtn = document.querySelector("#create");
+    createBtn.addEventListener('click', () =>{
+        var name = document.querySelector('input[name="name"]').value
+        var age = document.querySelector('input[name="age"]').value
+        var point = document.querySelector('input[name="point"]').value
+        
+        var formData = {
+            name: name,
+            age: age,
+            point: point
+        }
+
+        createStudent(formData)
+    })
+}
+
+function createStudent(data, callback){
+    var options = {
+        method: 'POST',
+        headers:{
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+    fetch(getApi, options)
+        .then(function(response){
+            response.json();
+        })
+        .then(callback)
 }
